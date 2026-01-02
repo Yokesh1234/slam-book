762e68c614b00110151c53141a4add7774efd2ee
Yokesh <133478337+Yokesh1234@users.noreply.github.com>
2026-01-02 14:39:50 +0530

    update

--- services/firebase.ts
+++ services/firebase.ts
@@ -3,13 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyBHrWVchcWOT31By3AtnEzvLxYy-VzrLKw",
+
   authDomain: "slam-book-app-85578.firebaseapp.com",
+
   projectId: "slam-book-app-85578",
-  storageBucket: "slam-book-app-85578.appspot.com", // ✅ FIXED
+
+  storageBucket: "slam-book-app-85578.firebasestorage.app",
+
   messagingSenderId: "685811264602",
+
   appId: "1:685811264602:web:ace07970a6098e91a87c63"
+
 };
+
 
 // Initialize Firebase safely (no duplicate app error)
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
